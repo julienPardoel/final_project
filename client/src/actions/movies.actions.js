4c4c99694f542ce7f3b1b2636a2d8c319a873ff5
DESKTOP-189F2TD\jux33 <julien.pardoel@gmail.com>
2021-04-15 16:16:08 +0200

    data api language fr

--- client/src/actions/movies.actions.js
+++ client/src/actions/movies.actions.js
@@ -5,7 +5,7 @@ export const GET_MOVIES = "GET_MOVIES";
 export const getMovies = () => {
     return (dispatch) => {
       return axios
-        .get(`https://api.themoviedb.org/3/movie/popular?api_key=fd4d4bf6cf58ba27b154b5975554d16a`)
+        .get(`https://api.themoviedb.org/3/movie/popular?api_key=fd4d4bf6cf58ba27b154b5975554d16a&language=fr`)
         .then((res) => {
             console.log(res.data.results);
           dispatch({ type: GET_MOVIES, payload: res.data.results });
